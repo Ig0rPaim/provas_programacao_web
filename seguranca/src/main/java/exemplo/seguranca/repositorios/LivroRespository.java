c5834bc6f912103c8715b5bae4263b74c94a775b
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-04-10 21:13:36 -0300

    settings querys

--- seguranca/src/main/java/exemplo/seguranca/repositorios/LivroRespository.java
+++ seguranca/src/main/java/exemplo/seguranca/repositorios/LivroRespository.java
@@ -14,10 +14,10 @@ public interface LivroRespository extends JpaRepository<Livro, Long>{
     public void emprestar(@Param("livro_id") Long livro_id, @Param("usuario_id") Long usuario_id, @Param("emprestimo") int emprestimo);
 
     
-    @Query("update livros u set u.quantidade = :qtd where u.id = :id_livro")
+    @Query("update livros u set u.quantidade = ?#{qtd} where u.id = ?#{id_livro}")
     public void mudarQuantidade(@Param("qtd") Long qtd, @Param("id_livro") Long id_livro);
 
-    @Query("update livros_usuarios u set u.emprestado = :emprestimo where u.id_livro = :livro_id and u.id_usuario = :usuario_id")
+    @Query("update livros_usuarios u set u.emprestado = ?#{emprestimo} where u.id_livro = ?#{livro_id} and u.id_usuario = ?#{usuario_id}")
     public void devolver(@Param("emprestimo") int emprestimo, @Param("id_livro") Long id_livro, @Param("usuario_id") Long usuario_id);
 
 }
